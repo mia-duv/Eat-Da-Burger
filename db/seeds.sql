a822605ff8dd85ca4616f8026ba1b02d279e4090
mia-duv <56006479+mia-duv@users.noreply.github.com>
2020-02-12 18:44:30 -0500

    adding app

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name) VALUES ('Classic Burger');
+INSERT INTO burgers (burger_name) VALUES ('Cheese Burger');
+INSERT INTO burgers (burger_name, devoured) VALUES ('Baconator', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Chimi Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cali Burger', true);
+INSERT INTO burgers (burger_name) VALUES ('Spicy Burger');
---
+++ public/assets/js/burgers.js
@@ -0,0 +1,54 @@
+$(function() {
+    $(".change-devoured").on("click", function(event) {
+      var id = $(this).data("id");
+      var newDevoured = $(this).data("newdevoured");
+  
+      var newDevouredState = {
+        devoured: newDevoured
+      };
+  
+      $.ajax("/api/burgers/" + id, {
+        type: "PUT",
+        data: newDevouredState
+      }).then(
+        function() {
+          console.log("changed burger to", newDevoured);
+          location.reload();
+        }
+      );
+    });
+  
+    $("#submitButton").on("click", function(event) {
+      console.log("I am here");
+      event.preventDefault();
+  
+      var newBurger = {
+        burger_name: $("#brgr").val().trim(),
+        devoured: 0
+        //  $("[burger_name=devoured]:checked").val()
+      };
+  
+    
+      $.ajax("/api/burgers", {
+        type: "POST",
+        data: newBurger
+      }).then(
+        function() {
+          console.log("new burger created");
+          location.reload();
+        }
+      );
+    });
+  
+    $(".delete-burger").on("click", function(event) {
+      var id = $(this).data("id");
+      $.ajax("/api/burgers/" + id, {
+        type: "DELETE"
+      }).then(
+        function() {
+          console.log("deleted burger", id);
+          location.reload();
+        }
+      );
+    });
+  });
